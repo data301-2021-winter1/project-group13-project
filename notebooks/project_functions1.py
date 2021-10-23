b9aab7b818ed727fcb2a578008fe1dd974a2b581
nordiCode.github.io <grantsbfi@gmail.com>
2021-10-23 16:18:50 -0700

    Combined loading and cleaning
    Of both csvs and csv.zips into single function

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -3,14 +3,18 @@ import pandas as pd
 import os # to get to document and open it
 from collections import Counter
 
-def loadCsvZipDf(csvFile):
-    with zipfile.ZipFile('../data/raw/survey_results_public_2019.csv.zip') as myzip:
-        data2019 = myzip.open('survey_results_public_2019.csv')
-    df = pd.read_csv(data2019,dtype='unicode')
-    return df
+def unprocessed(csvFile):
+    return pd.read_csv(csvFile)
 
+def load_and_process(csvFile):
+    lastThree = csvFile[-3:]
+    if(lastThree == 'zip'):
+        with zipfile.ZipFile('../data/raw/survey_results_public_2019.csv.zip') as myzip:
+            data2019 = myzip.open('survey_results_public_2019.csv')
+        df = pd.read_csv(data2019,dtype='unicode')
+    else:
+        df = pd.read_csv(csvFile)
 
-def dfCleaner(df):
     dfCleaned = df.copy().drop(['SOAccount','SOPartFreq','SurveyLength','SurveyEase'],axis=1)
     return dfCleaned
 
@@ -37,3 +41,6 @@ def dfLangCount(df, col):
 def countPrintLang(cnt,year):
     for lang in sorted(cnt):
         print(f"{cnt[lang]} people have worked with {lang} in {year} ")
+
+
+load_and_process('hello.zip')
