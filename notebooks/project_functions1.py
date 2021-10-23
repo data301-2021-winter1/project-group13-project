a9bbe2f612398cd4bb68fb658a74040fc3758b70
nordiCode.github.io <grantsbfi@gmail.com>
2021-10-23 13:32:47 -0700

    Analysis1
    More clean up and moving functions to the project functions. Deleted a bunch of analysis similar among the years. Just to show one year

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -7,3 +7,8 @@ def loadCsvZipDf(csvFile):
         data2019 = myzip.open('survey_results_public_2019.csv')
     df = pd.read_csv(data2019,dtype='unicode')
     return df
+
+
+def dfCleaner(df):
+    dfCleaned = df.copy().drop(['SOAccount','SOPartFreq','NEWSOSites','SurveyLength','SurveyEase'],axis=1)
+    return dfCleaned
